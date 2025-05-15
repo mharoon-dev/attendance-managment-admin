23e3bae96f79fe6f94967d87262fbf33b0916972
mharoon-dev <haroon.aghani@gmail.com>
2025-05-15 20:44:56 +0500

    add code

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,6 @@
 const url = 'https://school-managment-system-server.vercel.app/api/';
 // const url = 'http://localhost:3000/api/';
-
+///
 import axios from "axios";
 
 export const api = axios.create({
